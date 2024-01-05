1a00590a0a487ef0f06623dc8de9b3cb0dbac867
rovinstoll <dickstollgm@gmail.com>
2024-01-04 17:29:53 -0700

    fix the modal updates

--- app/assets/scripts/modules/Modal.js
+++ app/assets/scripts/modules/Modal.js
@@ -3,13 +3,11 @@ class Modal {
         this.injectHTML()
         this.modal = document.querySelector(".modal")
         this.closeIcon = document.querySelector(".modal__close")
-        this.openModalButtons = document.querySelectorAll(".open-modal")
         this.events()
     }
 
     events() {
-        // listen for open click
-        this.openModalButtons.forEach(el => el.addEventListener("click", e => this.openTheModal(e)))
+
         // listen for close click
         this.closeIcon.addEventListener("click", () => this.closeTheModal())
 
@@ -19,8 +17,7 @@ class Modal {
 
 
 
-    openTheModal(e) {
-        e.preventDefault()
+    openTheModal() {
         this.modal.classList.add("modal--is-visible")
 
     }
