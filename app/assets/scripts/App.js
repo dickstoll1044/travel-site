e460ac9a51a5095fdae776fe7faa63cfa4657f77
rovinstoll <dickstollgm@gmail.com>
2024-01-04 17:42:26 -0700

    refresh master from 50 code splitting

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,8 +3,6 @@ import MobileMenu from './modules/MobileMenu'
 import RevealOnScroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
 
-
-
 new StickyHeader()
 new RevealOnScroll(document.querySelectorAll(".feature-item"), 75)
 new RevealOnScroll(document.querySelectorAll(".testimonial"), 60)
@@ -14,15 +12,13 @@ let modal
 document.querySelectorAll(".open-modal").forEach(el => {
   el.addEventListener("click", e => {
     e.preventDefault()
-    if (typeof modal == "undefined" ) {
-      import('./modules/Modal').then(x => {
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
         modal = new x.default()
         setTimeout(() => modal.openTheModal(), 20)
-      }).catch(() => console.log("These was problem."))
-    }
-    else {
+      }).catch(() => console.log("There was a problem."))
+    } else {
       modal.openTheModal()
-
     }
   })
 })
